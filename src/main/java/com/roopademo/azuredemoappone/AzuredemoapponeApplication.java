99ce539515ebd136ce5ca2106aafdd1436b45e03
roopa <roopa.bhavik@gmail.com>
2023-12-10 15:32:43 -0500

    first commit

---
+++ src/main/java/com/roopademo/azuredemoappone/AzuredemoapponeApplication.java
@@ -0,0 +1,13 @@
+package com.roopademo.azuredemoappone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzuredemoapponeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzuredemoapponeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roopademo/azuredemoappone/AzuredemoapponeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roopademo.azuredemoappone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzuredemoapponeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
